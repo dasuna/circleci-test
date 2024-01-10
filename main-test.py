80b317e7f27dd2219ca079a1a66ff59b0cbe1d41
dasuna <dasunath88@gmail.com>
2024-01-10 12:45:40 +0530

    Add Subtract function

--- main-test.py
+++ main-test.py
@@ -5,5 +5,10 @@ def TestAdd():
         assert Add(2,3) == 5
         print("Add Function works correctly")
 
+def TestSub():
+	assert Subtract(7,4) == 3
+	print("Subtract Function works correctly")
+
 if __name__ == '__main__':
         TestAdd()
+	TestSub()
