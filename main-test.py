bf0847b0fe23600a08fbfc9f8f396180589f8291
dasuna <dasunath88@gmail.com>
2024-01-10 18:39:18 +0530

    first commit

--- main-test.py
+++ main-test.py
@@ -1,17 +1,17 @@
 # Import the Add function, and assert that it works correctly.
-from main import Add,subtract
+from main import Add
 
 def TestAdd():
         assert Add(2,3) == 5
         print("Add Function works correctly")
 
-def TestSub():
-	assert subtract(7,4) == 3
-	print("Subtract Function works correctly")
+#def TestSub():
+	#assert subtract(7,4) == 3
+	#print("Subtract Function works correctly")
 
 if __name__ == '__main__':
         TestAdd()
-        TestSub()
+        #TestSub()
 	
 
 
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def Add(a, b):
 def SayHello():
         print("sup world from srcmake")
 
-def subtract(a, b):
-	return a - b
+#def subtract(a, b):
+	#return a - b
 
 if __name__ == '__main__':
         SayHello()
